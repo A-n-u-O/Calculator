311f9c219d3076c1bad565c6072b525ed65455d1
A-n-u-O <anuvictor2005@gmail>
2024-02-01 23:54:36 +0100

    Javascript classes implemented

--- script.js
+++ script.js
@@ -1,24 +1,74 @@
-const container = document.querySelector('.container');
-const display = document.querySelector('.display');
-const button = document.querySelectorAll('button');
-const operator = document.querySelector('.operator');
-const specialChars = ["%", "*", "/", "+", "-", "="];
-let output = "";
-
-const calculate = (btnValue) => {
-    if(btnValue === "=" && output !== ''){
-        output = eval(output.replace("%", "/100"))
-    }else if(btnValue === "AC"){
-        output = ""
-    }else if(btnValue==="DEL"){
-        output = output.toString().slice(0, -1)
-    }else{
-        if(output === "" && specialChars.includes(btnValue))return;
-        output += btnValue;
+class Calculator{
+    constructor(previousInput, operator, currentInput, calculate){
+        this.previousInput = '';
+        this.operator = null;
+        this.currentInput = '0';
+        this.display = document.getElementById('display');
+        this.updateDisplay();
     }
-    display.value = output;
-    console.log(btnValue);
+    updateDisplay(){
+        this.display.value = this.currentInput;
+    }
+    clear(){
+        this.previousInput = '';
+        this.operator = null;
+        this.currentInput = '0';
+        this.updateDisplay();
+    }
+    delete(){
+        this.display.value = this.currentInput.slice(0, -1) || '0';
+        this.updateDisplay();
+    }
+
+    appendNumber(number){
+        if(this.currentInput ==='0' || this.currentInput === '-0'){
+            this.currentInput = number.toString();
+        }else {
+            this.currentInput += number.toString();
+        }
+        this.updateDisplay();
+    }
+    setDecimal(){
+        if(!this.currentInput.includes('.')){
+            this.currentInput += '.';
+            this.updateDisplay();
+        }
+    }
+    setOperation(operator){
+        if(this.currentInput !==  '' && this.currentInput !== '-'){
+            this.operator = operator;
+            this.previousInput = this.currentInput;
+            this.currentInput = '';
+            this.updateDisplay();
+        }
+    }
+    calculate(){
+        if(this.operator && this.previousInput !== '' && this.currentInput !== '' ){
+            const prev = parseFloat(this.previousInput);
+            const current = parseFloat(this.currentInput);
+            let result;
+
+            switch(this.operator){
+                case '+':
+                    result = prev + current;
+                    break;
+                case '-':
+                    result = prev - current;
+                    break;
+                case '*':
+                    result = prev * current;
+                    break;
+                case '/':
+                    result = prev / current;
+                    break;
+                default:
+                    break;
+
+            }
+            this.currentInput = result.toString();
+            this.previousInput = '';
+            this.operator = null;
+            this.updateDisplay();
+        }
+    }
 }
-button.forEach((button) => {
-    button.addEventListener("click", (e) => calculate(e.target.dataset.value))
-})
